47da9011e61133af2ca969e8d0246f957ee02d14
Nunnaphat Rujipoonpong <66160316@go.buu.ac.th>
2026-06-03 13:18:43 +0700

    refactor: remove redundant eslint disable directive from notification service

--- src/notification/notification.service.ts
+++ src/notification/notification.service.ts
@@ -1,4 +1,3 @@
-/* eslint-disable prettier/prettier */
 import { Injectable, Logger } from '@nestjs/common';
 import { Cron } from '@nestjs/schedule';
 import { InjectRepository } from '@nestjs/typeorm';
